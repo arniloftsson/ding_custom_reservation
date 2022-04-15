0cd71a9eb49bc06e099ae8c361e32d0d08cc4dac
Arni Loftsson <arniloftsson10.com>
2022-04-15 07:58:20 +0000

    Added trapping off focus in dialog

--- js/ding_custom_reservation.js
+++ js/ding_custom_reservation.js
@@ -4,5 +4,26 @@
   // The form is loaded via ajax so only after an ajax call can we set focus.
   $(document).ajaxComplete(function (e, xhr, settings) {
     $('#edit-provider-options-interest-period').focus();
+
+    var firstTabbable = $('.popupbar-close');
+    var lastTabbable = $('.reserve-button');
+
+    /*redirect last tab to first input*/
+    lastTabbable.on('keydown', function (e) {
+       if ((e.which === 9 && !e.shiftKey)) {
+         console.log("1");
+           e.preventDefault();
+           firstTabbable.focus();
+       }
+    });
+
+    /*redirect first shift+tab to last input*/
+    firstTabbable.on('keydown', function (e) {
+        if ((e.which === 9 && e.shiftKey)) {
+          console.log("2");
+            e.preventDefault();
+            lastTabbable.focus();
+        }
+    });
   });
 }(jQuery));
