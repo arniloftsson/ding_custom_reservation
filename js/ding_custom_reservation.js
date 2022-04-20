64fa55352f26dfbbe806e9ce1c24a83a7f8f0bb2
Arni Loftsson <arniloftsson10.com>
2022-04-20 10:40:58 +0000

    Tab is now not trapped in modal after close

--- js/ding_custom_reservation.js
+++ js/ding_custom_reservation.js
@@ -8,22 +8,29 @@
     var firstTabbable = $('.popupbar-close');
     var lastTabbable = $('.reserve-button');
 
+    var keydownHandlerLast = function (e) {
+      if ((e.which === 9 && !e.shiftKey)) {
+          e.preventDefault();
+          firstTabbable.focus();
+      }
+   }
+
+   var keydownHandlerFirst = function (e) {
+    if ((e.which === 9 && e.shiftKey)) {
+      e.preventDefault();
+      lastTabbable.focus();
+    }
+  };
+
     /*redirect last tab to first input*/
-    lastTabbable.on('keydown', function (e) {
-       if ((e.which === 9 && !e.shiftKey)) {
-         console.log("1");
-           e.preventDefault();
-           firstTabbable.focus();
-       }
-    });
+    lastTabbable.on('keydown', keydownHandlerLast);
 
     /*redirect first shift+tab to last input*/
-    firstTabbable.on('keydown', function (e) {
-        if ((e.which === 9 && e.shiftKey)) {
-          console.log("2");
-            e.preventDefault();
-            lastTabbable.focus();
-        }
+    firstTabbable.on('keydown', keydownHandlerFirst);
+
+    firstTabbable.click(function (e) {
+      firstTabbable.unbind('keydown', keydownHandlerFirst);
+      lastTabbable.unbind('keydown', keydownHandlerLast);
     });
   });
 }(jQuery));
